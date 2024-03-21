6d486fb93c9ed3193de426a402b1d748068f3e55
Theteu <matheusdesouzaoliveira32@gmail.com>
2024-03-20 21:42:39 -0300

    atualizacao

--- Exercicio 2/js.js
+++ Exercicio 2/js.js
@@ -1,39 +1,40 @@
-let alturaPedro = 1.5;//2.3
+let alturaPedro = 150;//2.3
                         //40
-let alturaLucas = 1.1;//2.3
+let alturaLucas = 110;//2.3
 
-const crescimentoPedro = 0.02;
+const crescimentoPedro = 2;
 
-const crescimentoLucas = 0.03;
+const crescimentoLucas = 3;
 
 let anos = 0;
 
 
-for (anos; alturaLucas <= alturaPedro; anos++){
+while (alturaLucas < alturaPedro){
     alturaLucas += crescimentoLucas;
     alturaPedro += crescimentoPedro;
+    anos++;
 }
 
 document.write(`Precisou de ${anos} para que lucas tenha a mesma altura de pedro. <br>
-altura Pedro: ${alturaPedro.toFixed(2)}m <br>
-altura Lucas: ${alturaPedro.toFixed(2)}m`  )
+altura Pedro: ${alturaPedro / 100}m <br>
+altura Lucas: ${alturaPedro / 100}m ` )
 
-// let altura_Pedro = 1.5; //2.32
-//                         //41
-// let altura_Lucas = 1.1;//2.33
+let altura_Pedro = 1.5; //2.32
+                        //41
+let altura_Lucas = 1.1;//2.33
 
-// const crescimento_Pedro = 0.02;
+const crescimento_Pedro = 0.02;
 
-// const crescimento_Lucas = 0.03;
+const crescimento_Lucas = 0.03;
 
-// let ano = 0;
+let ano = 0;
 
-// while (altura_Lucas < altura_Pedro) {
-//     ano++;
-//     altura_Lucas += crescimento_Lucas;
-//     altura_Pedro += crescimento_Pedro;
-// }
+while (altura_Lucas < altura_Pedro) {
+    ano++;
+    altura_Lucas += crescimento_Lucas;
+    altura_Pedro += crescimento_Pedro;
+}
 
-// document.write(`<br> Precisou de ${ano} anos para que Lucas ficasse maior que Pedro. <br>
-// altura Pedro: ${altura_Pedro.toFixed(2)}m <br>
-// altura Lucas: ${altura_Lucas.toFixed(2)}m`);
+document.write(`<br> Precisou de ${ano} anos para que Lucas ficasse maior que Pedro. <br>
+altura Pedro: ${altura_Pedro.toFixed(2)}m <br>
+altura Lucas: ${altura_Lucas.toFixed(2)}m`);
